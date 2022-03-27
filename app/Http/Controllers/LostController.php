4d83a687db0dce439eb99eac4e2b900753f60621
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-03-27 13:27:34 +0430

    Create Lost classes
    controller, model, requests, policy, factory and migration

---
+++ app/Http/Controllers/LostController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLostRequest;
+use App\Http\Requests\UpdateLostRequest;
+use App\Models\Lost;
+
+class LostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreLostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreLostRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lost  $lost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lost $lost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lost  $lost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lost $lost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateLostRequest  $request
+     * @param  \App\Models\Lost  $lost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateLostRequest $request, Lost $lost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lost  $lost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lost $lost)
+    {
+        //
+    }
+}
