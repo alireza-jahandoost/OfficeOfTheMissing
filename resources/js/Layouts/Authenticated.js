b7d41c57b1c32c7c903b791c1b11a10cdc9a431a
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-03-31 20:18:25 +0430

    Add a link to Licenses/Index.js in navbar

--- resources/js/Layouts/Authenticated.js
+++ resources/js/Layouts/Authenticated.js
@@ -24,6 +24,9 @@ export default function Authenticated({ auth, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('licenses.index')} active={route().current('licenses.index')}>
+                                    Licenses
+                                </NavLink>
                             </div>
                         </div>
 
