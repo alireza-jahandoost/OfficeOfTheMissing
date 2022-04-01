8f5b9d6f2470d80c6310652d75c058ab849c4031
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-04-01 15:58:39 +0430

    Implement Founds/IndexLicenses.js

--- resources/js/Pages/Founds/IndexLicenses.js
+++ resources/js/Pages/Founds/IndexLicenses.js
@@ -1,7 +1,27 @@
+import Authenticated from '@/Layouts/Authenticated';
+import {Head, Link} from '@inertiajs/inertia-react';
 import React from 'react';
 
-const IndexLicenses = () => {
-    return null;
+const IndexLicenses = ({auth, errors: authenticatedErrors, licenses}) => {
+    return (
+        <Authenticated
+            auth={auth}
+            errors={authenticatedErrors}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">مدارک پیدا شده</h2>}
+        >
+            <Head title="Licenses" />
+
+            <div className="m-10">
+                {licenses.map(license => (
+                    <div key={license.id}>
+                        <Link className="text-blue-800" href={route('licenses.founds.index', [license])}>
+                            {license.name}
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </Authenticated>
+    );
 }
 
 export default IndexLicenses;
